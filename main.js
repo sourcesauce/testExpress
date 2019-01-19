1237f7a6d830e32c4a373d7f43c2deadac19f169
소상훈 <sshlc940503@naver.com>
2019-01-19 10:24:10 +0900

    상세페이지 구현

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var path = require('path');
 var template = require('./lib/template.js');
+var sanitizeHtml = require('sanitize-html');
 
 app.get('/', function (req, res) {
   fs.readdir('./data', function(error, filelist){
@@ -13,10 +15,31 @@ app.get('/', function (req, res) {
       `<a href="/create">create</a>`
     );
     res.send(html);
+  });
 });
 
 app.get('/page/:pageId', function(req,res){
-  res.send(req.params);
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      var title = req.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+                    <a href="/update?id=${sanitizedTitle}">update</a>
+                    <form action="delete_process" method="post">
+                      <input type="hidden" name="id" value="${sanitizedTitle}">
+                      <input type="submit" value="delete">
+                    </form>`
+      );
+      res.send(html);
+    });
+  });
 });
 
 app.listen(3000, function () {
@@ -29,50 +52,17 @@ app.listen(3000, function () {
 // var url = require('url');
 // var qs = require('querystring');
 
-// var path = require('path');
-// var sanitizeHtml = require('sanitize-html');
+
+
 
 // var app = http.createServer(function(request,response){
 //     var _url = request.url;
 //     var queryData = url.parse(_url, true).query;
 //     var pathname = url.parse(_url, true).pathname;
 //     if(pathname === '/'){
-//       if(queryData.id === undefined){
-//         fs.readdir('./data', function(error, filelist){
-//           var title = 'Welcome';
-//           var description = 'Hello, Node.js';
-//           var list = template.list(filelist);
-//           var html = template.HTML(title, list,
-//             `<h2>${title}</h2>${description}`,
-//             `<a href="/create">create</a>`
-//           );
-//           response.writeHead(200);
-//           response.end(html);
-//         });
+//       
 //       } else {
-//         fs.readdir('./data', function(error, filelist){
-//           var filteredId = path.parse(queryData.id).base;
-//           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-//             var title = queryData.id;
-//             var sanitizedTitle = sanitizeHtml(title);
-//             var sanitizedDescription = sanitizeHtml(description, {
-//               allowedTags:['h1']
-//             });
-//             var list = template.list(filelist);
-//             var html = template.HTML(sanitizedTitle, list,
-//               `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-//               ` <a href="/create">create</a>
-//                 <a href="/update?id=${sanitizedTitle}">update</a>
-//                 <form action="delete_process" method="post">
-//                   <input type="hidden" name="id" value="${sanitizedTitle}">
-//                   <input type="submit" value="delete">
-//                 </form>`
-//             );
-//             response.writeHead(200);
-//             response.end(html);
-//           });
-//         });
-//       }
+//         
 //     } else if(pathname === '/create'){
 //       fs.readdir('./data', function(error, filelist){
 //         var title = 'WEB - create';
